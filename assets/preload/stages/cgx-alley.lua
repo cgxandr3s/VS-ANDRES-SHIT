e241267a9b4b8ff9e20d8348ab6a33356e5e4f13
cgxandr3s <94736268+cgxandr3s@users.noreply.github.com>
2021-12-03 19:47:55 -0500

    CODE
    code and fl zip and chromatic

---
+++ assets/preload/stages/cgx-alley.lua
@@ -0,0 +1,7 @@
+function onCreate()
+
+	makeLuaSprite('alley', 'cgx-alley', -399, -70);
+	addLuaSprite('alley',false)
+
+	close(true)
+end
---
+++ assets/preload/stages/sonicFUN.lua
@@ -0,0 +1,7 @@
+function onCreate()
+
+makeLuaSprite('back', 'sonicFUNsky', -650, -300)
+addLuaSprite('back',false)
+
+	close(true)
+end
